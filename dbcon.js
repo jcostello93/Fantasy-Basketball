a0c7b81b85f903a769d41a7acafea0456ac17b45
jcostello93 <costejoh@oregonstate.edu>
2018-07-07 23:08:33 +0200

    Delete dbcon.js

--- dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'localhost',
-  user            : 'root',
-  password        : 'nyknicks1',
-  database        : 'nba'
-});
-module.exports.pool = pool;
